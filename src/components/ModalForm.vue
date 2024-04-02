f097e88cf2bc7366d4f759954a6308374777a372
Aruzhan Abdurahim <100573843+aabdurahim@users.noreply.github.com>
2024-04-02 13:49:05 +0600

    Update ModalForm.vue

--- src/components/ModalForm.vue
+++ src/components/ModalForm.vue
@@ -4,8 +4,8 @@
     <div class="container mx-auto md:w-3/5 px-3 py-10 bg-white rounded-[20px]">
       <form @submit.prevent="createOrUpdateTask" class="w-1/2 mx-auto justify-center items-center flex flex-wrap mt-5">
         <h2 class="text-center text-3xl font-medium">{{ taskToUpdate ? 'Edit current task' : 'Add new task' }}</h2> 
-        <input v-model="taskTitle" type="text" placeholder="Заголовок" class="border border-b-[#01002e] p-3 mt-10 w-full">
-        <input v-model="taskDescription" type="text" placeholder="Описание" class="border border-b-[#01002e] p-3 mt-10 w-full">
+        <input v-model="taskTitle" type="text" placeholder="Title" class="border border-b-[#01002e] p-3 mt-10 w-full">
+        <input v-model="taskDescription" type="text" placeholder="Description" class="border border-b-[#01002e] p-3 mt-10 w-full">
         <button type="submit" class="text-2xl font-medium text-[#01002e] bg-[#5ebfdf] bg-opacity-50 hover:bg-opacity-100 hover:text-white px-7 py-2 mt-10 rounded-[10px]">
           {{ taskToUpdate ? 'Update' : 'Add' }}
         </button>
